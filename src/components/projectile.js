30ece851072498ae5221c5e339588db8f3d15833
xpolb1 <bogdan.polovko@gmail.com>
2017-01-20 22:05:59 -0500

    Implemented the collision handling for bullet

--- src/components/projectile.js
+++ src/components/projectile.js
@@ -1,9 +1,51 @@
 AFRAME.registerComponent('projectile', {
   schema: {
-    speed: { default: -0.4 }
+    speed: { default: -0.4 },
+    target: { default: '.enemy' }
   },
-
+  init: function () {
+      let enemies = document.querySelectorAll('.enemy')
+      this.targets = [];
+      this.ticks = 0;
+      for (var i = 0; i < enemies.length; i++) {
+        this.targets.push(enemies[i].object3D);
+      }
+    },
+  remove: function() {
+    this.el.removeObject3D('projectile');
+  },
   tick: function () {
-    this.el.object3D.translateY(this.data.speed);
+
+    let intersect = (sphere, box) => {
+      // get box closest point to sphere center by clamping
+      var x = Math.max(box.minX, Math.min(sphere.x, box.maxX));
+      var y = Math.max(box.minY, Math.min(sphere.y, box.maxY));
+      var z = Math.max(box.minZ, Math.min(sphere.z, box.maxZ));
+
+      // this is the same as isPointInsideSphere
+      var distance = Math.sqrt((x - sphere.x) * (x - sphere.x) +
+                               (y - sphere.y) * (y - sphere.y) +
+                               (z - sphere.z) * (z - sphere.z));
+      return distance < 0.15;
+    }
+    let el = this.el;
+    if(this.targets[0] && el.parentEl) {
+      this.targets[0].position
+      let box = {
+        minX: this.targets[0].position.x - 2,
+        minY: this.targets[0].position.y - 2,
+        minZ: this.targets[0].position.z - 2,
+        maxX: this.targets[0].position.x + 2,
+        maxY: this.targets[0].position.y + 2,
+        maxZ: this.targets[0].position.z + 2
+      }
+      let sphere = this.el.object3D.translateY(this.data.speed).position
+      if(intersect(sphere, box)) {
+        let target = document.querySelector('.enemy');
+        target.parentNode.removeChild(target)
+        el.parentNode.removeChild(el)
+      }
+    }
+
   }
 });
--- src/components/spawner.js
+++ src/components/spawner.js
@@ -3,29 +3,31 @@ AFRAME.registerComponent('spawner', {
     on: { default: 'click' },
     mixin: { default: '' }
   },
-
   /**
-   * Update event listener.
+   * Add event listener.
    */
   update: function (oldData) {
     this.el.addEventListener(this.data.on, this.spawn.bind(this));
   },
-
   /**
    * Spawn new entity at entity's current position.
    */
   spawn: function () {
     var el = this.el;
     var entity = document.createElement('a-entity');
-    var entityRotation;
     var matrixWorld = el.object3D.matrixWorld;
     var position = new THREE.Vector3();
-    var rotation = this.el.getAttribute('rotation');
-
+    var rotation = el.getAttribute('rotation');
+    var entityRotation;
+    position.setFromMatrixPosition(matrixWorld);
+    entity.setAttribute('position', position);
+    // Have the spawned entity face the same direction as the entity.
+    // Allow the entity to further modify the inherited rotation.
     position.setFromMatrixPosition(matrixWorld);
     entity.setAttribute('position', position);
     entity.setAttribute('mixin', this.data.mixin);
     entity.addEventListener('loaded', function () {
+      //changed getDeprecatedAttribute to getAttribute
       entityRotation = entity.getAttribute('rotation');
       entity.setAttribute('rotation', {
         x: entityRotation.x + rotation.x,
@@ -34,6 +36,5 @@ AFRAME.registerComponent('spawner', {
       });
     });
     el.sceneEl.appendChild(entity);
-    entity.play();
   }
 });
