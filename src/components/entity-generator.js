bf4458ecf73a87a8e98b4dc757c2bfd4cda0ccf8
xpolb1 <bogdan.polovko@gmail.com>
2017-01-22 14:48:45 -0500

    Generates new boxes after they were destroyed

--- src/components/entity-generator.js
+++ src/components/entity-generator.js
@@ -6,14 +6,19 @@ AFRAME.registerComponent('entity-generator', {
 
   init: function () {
     var data = this.data;
-
     // Create entities with supplied mixin.
     for (var i = 0; i < data.num; i++) {
       var entity = document.createElement('a-entity');
       entity.setAttribute('mixin', data.mixin);
       entity.setAttribute('class', 'enemy')
-
       this.el.appendChild(entity);
+    }
+  },
+  tick: function() {
+    let enemies = document.querySelectorAll('.enemy');
+
+    if(!enemies.length) {
+      this.init()
     }
   }
 });
