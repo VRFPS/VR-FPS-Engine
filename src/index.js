e6d0aa8daa72b69c75e3aa1aa7e78b6c8a695a9d
dye784 <damon.ye91@gmail.com>
2017-01-23 10:54:17 -0500

    almost there

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ window.CABBAGES = {
   currentScore: 0
 }
 
-// require('aframe');
-// require('aframe-randomizer-components');
+require('aframe');
+require('aframe-randomizer-components');
 require('./components/mobileShootButton')
 require('./components/entity-generator');
 require('./components/click-listener');
