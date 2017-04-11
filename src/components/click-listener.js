a284cbb07839956dbc3e2c4a55cf48ac849679ad
dye784 <damon.ye91@gmail.com>
2017-04-11 16:20:40 -0400

    refactored for ES6

--- src/components/click-listener.js
+++ src/components/click-listener.js
@@ -1,15 +1,14 @@
 AFRAME.registerComponent('click-listener', {
- init: function () {
-   var el = this.el;
-   if (AFRAME.utils.device.isMobile()) {
-     window.addEventListener('click', function (evt) {
-       el.emit('click', null, false)
-     })
-   } else {
-     window.addEventListener('click', function (evt) {
+  init() {
+    const el = this.el;
+    if (AFRAME.utils.device.isMobile()) {
+     window.addEventListener('click', (evt) => {
        el.emit('click', null, false);
      });
-   }
- }
-
+    } else {
+     window.addEventListener('click', (evt) => {
+       el.emit('click', null, false);
+     });
+    }
+  }
 });
