b4d51a0c637a486a226bec63b7fb2eff49322b43
dye784 <damon.ye91@gmail.com>
2017-04-11 16:41:45 -0400

    syntax

--- src/components/projectile.js
+++ src/components/projectile.js
@@ -15,23 +15,24 @@ AFRAME.registerComponent('projectile', {
 
   tick() {
     const intersect = (sphere, box) => {
-      // get box closest point to sphere center by clamping
-    const x = Math.max(box.minX, Math.min(sphere.x, box.maxX));
-    const y = Math.max(box.minY, Math.min(sphere.y, box.maxY));
-    const z = Math.max(box.minZ, Math.min(sphere.z, box.maxZ));
+        // get box closest point to sphere center by clamping
+      const x = Math.max(box.minX, Math.min(sphere.x, box.maxX));
+      const y = Math.max(box.minY, Math.min(sphere.y, box.maxY));
+      const z = Math.max(box.minZ, Math.min(sphere.z, box.maxZ));
 
-    // this is the same as isPointInsideSphere
-    const distance = Math.sqrt((x - sphere.x) * (x - sphere.x) +
-                             (y - sphere.y) * (y - sphere.y) +
-                             (z - sphere.z) * (z - sphere.z));
-    return distance < 0.25;
-  };
+      // this is the same as isPointInsideSphere
+      const distance = Math.sqrt((x - sphere.x) * (x - sphere.x) +
+                               (y - sphere.y) * (y - sphere.y) +
+                               (z - sphere.z) * (z - sphere.z));
+      return distance < 0.25;
+    };
 
     const bullet = this.el;
+
     if (bullet.object3D.position.length() > 100 && bullet.parentEl) {
       bullet.parentNode.removeChild(bullet);
     } else if (this.targets.length !== 0 && bullet.parentEl) {
-      for (let i = 0; i < this.targets.length; i++ ) {
+      for (let i = 0; i < this.targets.length; i++) {
         const currentEnemy = this.targets[i].object3D;
 
         const box = {
@@ -40,7 +41,7 @@ AFRAME.registerComponent('projectile', {
           minZ: currentEnemy.position.z - 2,
           maxX: currentEnemy.position.x + 2,
           maxY: currentEnemy.position.y + 2,
-          maxZ: currentEnemy.position.z + 2
+          maxZ: currentEnemy.position.z + 2,
         };
 
         const sphere = bullet.object3D.translateY(this.data.speed).position;
